7f533c484d47c9b1c715bacd66407baddf7b3484
Thel-nny <thelanny.maguillano-22@cpu.edu.ph>
2024-02-16 22:34:45 +0800

    Started the crud (create & read) to be reviewed

--- index.ts
+++ index.ts
@@ -1,17 +1,41 @@
+import { Pool } from "pg";
+
 const express = require('express')
 const app = express()
 
-app.post('/', (req, res) => {
-    res.send('Hello World')
+export const database = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'lab2_testing',
+    password: '103099',
+    port: 5432
+  })
+  
+app.post('/pogs', async (req, res, data) => {
+    try{
+      const query = await database.query('INSERT INTO pogs (name, ticker_symbol, price, color) VALUES ($1, $2, $3, $4)')
+      const Values = [data.name, data.ticker_symbol, data.price, data.color]
+    }catch (err){
+      console.error(err);
+      res.status(422).json({error:'An error occurred'});
+    }
 })
 
 
-app.get('/', (req, res) => {
+app.get('/pogs', async (req, res, data) => {
+  try {
+    const { id } = req.params;
+    const result = await database.query('SELECT * FROM pogs');
+    const result2 = await database.query('SELECT * FROM pogs WHERE id = $1', [id])
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-})
 
-
-app.patch('/', (req, res) => {
+app.patch('/pogs', (req, res) => {
 
 })
 
